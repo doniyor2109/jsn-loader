8110f5b3b59a0207d4ca89a69ed6f2c67fe60ddd
doniyor <doniyor2109@mail.ru>
2017-06-21 17:18:13 +0500

    initial files

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = function (source) {
+    var context = this._module && this._module.issuer
+        && this._module.issuer.context || "";
+    var filename = path.basename(this.resource).replace(/\..*?$/,".json");
+    var code = null;
+    try {
+        code = eval(source);
+    } catch (e){
+        console.error("wrong js for json format");
+    }
+    fs.writeFileSync(path.resolve(this.options.output.path, filename), JSON.stringify(code || {}));
+    return `module.exports = __webpack_public_path__ +  '${filename}'`;
+};
+
